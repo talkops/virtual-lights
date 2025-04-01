533c4bc527eef9c63874965ea39f2972b990e2b9
bierdok <2333728+bierdok@users.noreply.github.com>
2025-04-01 10:39:44 +0200

    Upgrade SDK + related changes

---
+++ README.md
@@ -0,0 +1,15 @@
+# TalkOps Extension: Virtual Lights
+
+A TalkOps Extension made to work with [TalkOps](https://talkops.app).
+
+**TalkOps enables real-time voice interaction with applications.**
+
+## Features
+
+* Turn on/off virtual lights
+
+## Installation
+
+1. Sign in to [TalkOps](https://talkops.app).
+2. Search for the `Virtual Lights` extension.
+3. Add the extension and follow the instructions.
--- index.mjs
+++ index.mjs
@@ -1,4 +1,38 @@
-import { Extension } from 'talkops'
+import { Extension, Parameter } from 'talkops'
+import update_lights from './schemas/functions/update_lights.json' with { type: 'json' }
 
-// Documentation: https://www.npmjs.com/package/talkops
-const extension = new Extension().setName('Boilerplate NodeJS')
+const states = new Map()
+
+const names = new Parameter('LIGHT_NAMES')
+  .setDescription('The names of the lights.')
+  .setDefaultValue('Desk lamp,Bedroom ceiling light')
+
+const extension = new Extension()
+  .setName('Virtual Lights')
+  .setIcon('https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678078-light-bulb-512.png')
+  .setCategory('home_automation')
+  .setDemo(true)
+  .setFeatures(['Turn on/off virtual lights'])
+  .setParameters([names])
+  .setFunctionSchemas([update_lights])
+  .setFunctions([
+    function update_lights(names, action) {
+      for (const name of names) {
+        states.set(name, action)
+      }
+      return 'Done.'
+    },
+  ])
+  .setBootstrap(loop)
+
+function loop() {
+  const lights = []
+  for (const name of names.getValue().split(',')) {
+    if (!states.has(name)) states.set(name, 'off')
+    lights.push(`"${name}" (${states.get(name)})`)
+  }
+  extension.setInstructions(
+    `You are a home automation assistant, you can manage the lights: ${lights.join(', ')}.`,
+  )
+  setTimeout(loop, 1000)
+}
